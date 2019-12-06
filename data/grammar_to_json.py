4fc0e36c1a5338acdf08aee6c90bd00028dfd422
Sai <svv232@nyu.edu>
2019-12-06 17:36:19 -0500

    need to fix edge cases, and regex cases before starting fzero fuzzing but have a json representation of the grammr, grammar needs to be slightly more robust, and modified for practical fuzzing

--- data/grammar_to_json.py
+++ data/grammar_to_json.py
@@ -1,6 +1,33 @@
-#!/usr/bin/env python3.
-from json import dumps, loads
-from collections import defaultdict as de_dict
+#!/usr/bin/env python3.7
+from json import dumps
+from collections import defaultdict
 
-with open("grammar.txt", "r") as grammar:
-    
+def pretty_print(gdict):
+    for k,v in gdict.items():
+        print(f"{k} = {v}")
+
+def transform(x):
+    x = x.strip()
+    if len(x) >= 1:
+        if x[0] != '"':
+            x = f"<{x}>"
+    return x
+
+def parse_grammar():
+    with open("grammar.txt", "r") as grammar:
+        gdict = defaultdict(list)
+        for l in grammar:
+            label, gramm = l.split(": ")
+            lexed = map(lambda x: transform(x), gramm.split(" "))
+            lexed = [(list(lexed)), ]
+            gdict[f"<{label}>"].append(lexed)
+    return gdict
+
+def serialize_grammar(gdict):
+    with open("grammar.json", "w") as jg:
+        js = dumps(gdict, indent=1)
+        jg.write(js)
+        
+if __name__ == "__main__":
+    gdict = parse_grammar()
+    serialize_grammar(gdict)
