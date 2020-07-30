e97abf723a9b2db9dfb0e69e9c58a9a3be9fe5d6
Brad Larsen <brad.larsen@trailofbits.com>
2020-07-30 11:53:43 -0400

    Fix warning about unused parameter

--- lib/Util/DefUse.cpp
+++ lib/Util/DefUse.cpp
@@ -6,7 +6,7 @@ namespace hyde {
 
 User::~User(void) {}
 
-void User::Update(uint64_t next_timestamp) {}
+void User::Update(uint64_t next_timestamp) { (void)next_timestamp; }
 
 uint64_t User::gNextTimestamp{1};
 
