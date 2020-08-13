d9912530d34b9fa60e99e4ac8b40043bc0ef9ed2
Eric Kilmer <eric.d.kilmer@gmail.com>
2020-08-13 15:37:41 -0400

    WIP: Add CMake install targets (#64)
    * Add install targets to each component
    
    * Adjust installation directory for libraries
    
    * Install header files
    
    * Install includes with each library CMakeLists
    
    * Make a function for library definition to cut down on copypasta
    
    * Generate export targets cmake files
    
    * Install in CI
    
    * Set install prefix

---
+++ cmake/define_library.cmake
@@ -0,0 +1,57 @@
+function(DefineLibrary libname)
+    set(options)
+    set(oneValueArgs CURDIR)
+    set(multiValueArgs SOURCES PUBLIC_HEADERS DEPENDENCIES PRIVATE_DEPS)
+    cmake_parse_arguments(CURLIB
+        "${options}"
+        "${oneValueArgs}"
+        "${multiValueArgs}"
+        ${ARGN})
+
+    string(TOLOWER ${PROJECT_NAME} lower_project_name)
+
+    # Create our library
+    add_library(${libname} STATIC
+        ${CURLIB_PUBLIC_HEADERS}
+        ${CURLIB_SOURCES}
+    )
+
+    add_library(${PROJECT_NAME}::${libname} ALIAS ${libname})
+
+    if(CURLIB_DEPENDENCIES)
+        target_link_libraries(${libname}
+            PUBLIC ${CURLIB_DEPENDENCIES}
+        )
+    endif()
+    if(CURLIB_PRIVATE_DEPENDENCIES)
+        target_link_libraries(${libname}
+            PRIVATE ${CURLIB_PRIVATE_DEPENDENCIES}
+        )
+    endif()
+
+    set_target_properties(${libname} PROPERTIES PUBLIC_HEADER "${CURLIB_PUBLIC_HEADERS}")
+    target_include_directories(${libname} PUBLIC
+      $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>
+      $<INSTALL_INTERFACE:include>
+      PRIVATE ${CURLIB_CURDIR}
+    )
+    install(
+      TARGETS ${libname}
+      EXPORT "${libname}Targets"
+      RUNTIME
+        DESTINATION "bin"
+      LIBRARY
+        DESTINATION "lib/${lower_project_name}"
+      ARCHIVE
+        DESTINATION "lib/${lower_project_name}"
+      PUBLIC_HEADER
+        DESTINATION "include/${lower_project_name}/${libname}"
+    )
+
+    install (
+        EXPORT      "${libname}Targets"
+        FILE        "${libname}Targets.cmake"
+        DESTINATION "lib/cmake/${PROJECT_NAME}"
+        NAMESPACE   "${PROJECT_NAME}::"
+    )
+endfunction()
