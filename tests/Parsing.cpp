c196e4eaf3d0fa47f8d86838812e6240f80a36bd
Brad Larsen <brad.larsen@trailofbits.com>
2020-09-04 18:49:06 -0400

    Add additional Dr. Lojekyll parsing test cases based on the examples directory.
    Some of those examples are intended to produce parse or compile errors;
    the majority, however, are meant to be real examples.
    
    These added tests attempt to check those intended behaviors.
    This does not work quite right at present.

--- tests/Parsing.cpp
+++ tests/Parsing.cpp
@@ -1,18 +1,65 @@
-#include <string>
 #include <filesystem>
+#include <string>
 
 #include "unittests.h"
 
+#include "drlojekyll/Display/DisplayConfiguration.h"
+#include "drlojekyll/Display/DisplayManager.h"
+#include "drlojekyll/Parse/ErrorLog.h"
+#include "drlojekyll/Parse/Parser.h"
+
 namespace fs = std::filesystem;
 
-// Make sure that `drlojekyll` can parse each of the .dr files in the examples
-// directory without an error
+namespace hyde {
+  // Needed for Google Test to be able to print hyde::ErrorLog values
+  std::ostream& operator<<(std::ostream& os, const ErrorLog& log) {
+    log.Render(os);
+    return os;
+  }
+}  // namespace hyde
+
+// Make sure that we can parse each of the .dr files in the examples directory
+// without an error
 TEST(Parsing, examples) {
   for (const auto &entry : fs::directory_iterator(kExamplesDir)) {
     if (entry.path().extension() != ".dr" || !fs::is_regular_file(entry)) {
       continue;
     }
-    // TODO(bjl): do some tests with each of the example files
-    // EXPECT_EQ(entry.path(), "hello");
+
+    SCOPED_TRACE(entry.path());
+
+    hyde::DisplayManager display_mgr;
+    hyde::ErrorLog err_log(display_mgr);
+    hyde::Parser parser(display_mgr, err_log);
+    hyde::DisplayConfiguration display_cfg = {entry.path(), 2, true};
+
+    auto mmod = parser.ParsePath(entry.path().native(), display_cfg);
+    EXPECT_TRUE(mmod.has_value());
+    EXPECT_TRUE(err_log.IsEmpty()) << "Parsing failed:" << std::endl << err_log;
+  }
+}
+
+// Make sure that we fail to parse each of the .dr files in the invalid_examples
+// directory with an error
+TEST(Parsing, invalid_examples) {
+  for (const auto &entry : fs::directory_iterator(kInvalidExamplesDir)) {
+    if (entry.path().extension() != ".dr" || !fs::is_regular_file(entry)) {
+      continue;
+    }
+
+    SCOPED_TRACE(entry.path());
+
+    // Try parsing the file.
+    hyde::DisplayManager display_mgr;
+    hyde::ErrorLog err_log(display_mgr);
+    hyde::Parser parser(display_mgr, err_log);
+    hyde::DisplayConfiguration display_cfg = {entry.path(), 2, true};
+
+    auto mmod = parser.ParsePath(entry.path().native(), display_cfg);
+    // NOTE: sometimes, parsing successfully gives back a module, but has
+    //       errors for other reasons.  So mmod may have a value.
+    //       But either we should get no module value, or we should get errors
+    //       in the log.
+    EXPECT_TRUE(!mmod.has_value() || !err_log.IsEmpty());
   }
 }
