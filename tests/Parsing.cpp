17de66dec2f7fa74336bcc311ce859c0edee4959
Eric Kilmer <eric.d.kilmer@gmail.com>
2020-11-03 17:04:24 -0500

    Refactor tests to use parameterized testsuite

--- tests/Parsing.cpp
+++ tests/Parsing.cpp
@@ -23,46 +23,52 @@ std::ostream &operator<<(std::ostream &os, const ErrorLog &log) {
 
 // Make sure that we can parse each of the .dr files in the examples directory
 // without an error
-TEST(Parsing, examples) {
-  for (const auto &entry : fs::directory_iterator(kExamplesDir)) {
-    if (entry.path().extension() != ".dr" || !fs::is_regular_file(entry)) {
-      continue;
+static auto DrFilesInDir(const fs::path &dir) {
+  std::vector<fs::path> all_entries;
+  for (const auto &entry : fs::directory_iterator(dir)) {
+    if (entry.path().extension() == ".dr" && fs::is_regular_file(entry)) {
+      all_entries.emplace_back(entry.path());
     }
-
-    SCOPED_TRACE(entry.path());
-
-    hyde::DisplayManager display_mgr;
-    hyde::ErrorLog err_log(display_mgr);
-    hyde::Parser parser(display_mgr, err_log);
-    std::string path(entry.path().string());
-    hyde::DisplayConfiguration display_cfg = {path, 2, true};
-
-    auto mmod = parser.ParsePath(path, display_cfg);
-    EXPECT_TRUE(mmod.has_value());
-    EXPECT_TRUE(err_log.IsEmpty()) << "Parsing failed:" << std::endl << err_log;
   }
+  return all_entries;
 }
+
+class PassingExamplesParsingSuite : public testing::TestWithParam<fs::path> {};
+
+TEST_P(PassingExamplesParsingSuite, Examples) {
+  auto path = GetParam().string();
+
+  hyde::DisplayManager display_mgr;
+  hyde::ErrorLog err_log(display_mgr);
+  hyde::Parser parser(display_mgr, err_log);
+  hyde::DisplayConfiguration display_cfg = {path, 2, true};
+
+  auto mmod = parser.ParsePath(path, display_cfg);
+  EXPECT_TRUE(mmod.has_value());
+  EXPECT_TRUE(err_log.IsEmpty()) << "Parsing failed:" << std::endl << err_log;
+}
+
+INSTANTIATE_TEST_SUITE_P(ValidExampleParsing, PassingExamplesParsingSuite,
+                         testing::ValuesIn(DrFilesInDir(kExamplesDir)));
+
+class FailingExamplesParsingSuite : public testing::TestWithParam<fs::path> {};
 
 // Make sure that we fail to parse each of the .dr files in the invalid_examples
 // directory with an error
-TEST(Parsing, invalid_syntax_examples) {
-  for (const auto &entry : fs::directory_iterator(kInvalidSyntaxExamplesDir)) {
-    if (entry.path().extension() != ".dr" || !fs::is_regular_file(entry)) {
-      continue;
-    }
+TEST_P(FailingExamplesParsingSuite, Examples) {
+  auto path = GetParam().string();
 
-    SCOPED_TRACE(entry.path());
+  hyde::DisplayManager display_mgr;
+  hyde::ErrorLog err_log(display_mgr);
+  hyde::Parser parser(display_mgr, err_log);
+  hyde::DisplayConfiguration display_cfg = {path, 2, true};
 
-    // Try parsing the file.
-    hyde::DisplayManager display_mgr;
-    hyde::ErrorLog err_log(display_mgr);
-    hyde::Parser parser(display_mgr, err_log);
-    std::string path(entry.path().string());
-    hyde::DisplayConfiguration display_cfg = {path, 2, true};
-
-    // Parsing is expected to fail for the invalid examples.
-    auto mmod = parser.ParsePath(path, display_cfg);
-    EXPECT_FALSE(mmod.has_value());
-    EXPECT_FALSE(err_log.IsEmpty());
-  }
+  // Parsing is expected to fail for the invalid examples.
+  auto mmod = parser.ParsePath(path, display_cfg);
+  EXPECT_FALSE(mmod.has_value());
+  EXPECT_FALSE(err_log.IsEmpty());
 }
+
+INSTANTIATE_TEST_SUITE_P(
+    InvalidSyntaxParsing, FailingExamplesParsingSuite,
+    testing::ValuesIn(DrFilesInDir(kInvalidSyntaxExamplesDir)));
