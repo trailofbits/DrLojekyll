570ca322d3da1c5295b47d330e6c963cac054d34
Eric Kilmer <eric.d.kilmer@gmail.com>
2020-11-18 21:28:07 -0500

    Naming conventions

--- tests/Parsing.cpp
+++ tests/Parsing.cpp
@@ -43,7 +43,7 @@ static auto DrFilesInDir(const fs::path &dir) {
 class PassingExamplesParsingSuite : public testing::TestWithParam<fs::path> {};
 
 // Set of examples that can parse but fail to build
-static std::unordered_set<std::string> BuildFailExamples{
+static const std::unordered_set<std::string> gBuildFailExamples{
     "min_block.dr", "pairwise_average_weight.dr", "function_counter.dr",
     "watched_user_dir.dr", "average_weight.dr"};
 
@@ -75,8 +75,8 @@ TEST_P(PassingExamplesParsingSuite, Examples) {
     std::optional<class hyde::Program> program_opt;
 
     // Some tests fail this step
-    if (BuildFailExamples.find(fs::path(path).filename().string()) !=
-        BuildFailExamples.end()) {
+    if (gBuildFailExamples.find(fs::path(path).filename().string()) !=
+        gBuildFailExamples.end()) {
       ASSERT_DEBUG_DEATH(
           program_opt = program_build_lambda(*query_opt, err_log), ".*TODO.*");
     } else {
