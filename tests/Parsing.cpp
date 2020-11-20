74c7eaa0efe4b0ac01465da492e27acfb533e4da
Eric Kilmer <eric.d.kilmer@gmail.com>
2020-11-20 13:29:36 -0500

    Tests for building IR (#126)
    * Tests building IR
    
    Passes without sanitizers. Leaks in DefUse
    
    * Naming conventions
    
    * Use set count() instead of find to skip known failures
    
    * No leak with always Clear and Erase
    
    * Save generated IR in tests to a file in build directory

--- tests/Parsing.cpp
+++ tests/Parsing.cpp
@@ -1,15 +1,22 @@
 // Copyright 2020, Trail of Bits, Inc. All rights reserved.
 
+#include <gtest/gtest.h>
+
 #include <filesystem>
+#include <fstream>
 #include <string>
+#include <unordered_set>
 
 #include "UnitTests.h"
 #include "drlojekyll/CodeGen/MessageSerialization.h"
+#include "drlojekyll/ControlFlow/Format.h"
 #include "drlojekyll/Display/DisplayConfiguration.h"
 #include "drlojekyll/Display/DisplayManager.h"
+#include "drlojekyll/Display/Format.h"
 #include "drlojekyll/Parse/ErrorLog.h"
 #include "drlojekyll/Parse/ModuleIterator.h"
 #include "drlojekyll/Parse/Parser.h"
+
 
 namespace fs = std::filesystem;
 
@@ -20,6 +27,15 @@ std::ostream &operator<<(std::ostream &os, const ErrorLog &log) {
   log.Render(os);
   return os;
 }
+
+struct FileStream {
+  FileStream(hyde::DisplayManager &dm_, const fs::path path_)
+      : fs(path_),
+        os(dm_, fs) {}
+
+  std::ofstream fs;
+  hyde::OutputStream os;
+};
 
 }  // namespace hyde
 
@@ -35,7 +51,20 @@ static auto DrFilesInDir(const fs::path &dir) {
   return all_entries;
 }
 
-class PassingExamplesParsingSuite : public testing::TestWithParam<fs::path> {};
+class PassingExamplesParsingSuite : public testing::TestWithParam<fs::path> {
+ protected:
+  void SetUp() override {
+    TestWithParam<fs::path>::SetUp();  // Sets up the base fixture first.
+    fs::create_directories(kGeneratedFilesDir);
+    SUCCEED() << "Created directory for generated test files: "
+              << kGeneratedFilesDir;
+  }
+};
+
+// Set of examples that can parse but fail to build
+static const std::unordered_set<std::string> gBuildFailExamples{
+    "min_block.dr", "pairwise_average_weight.dr", "function_counter.dr",
+    "watched_user_dir.dr", "average_weight.dr"};
 
 TEST_P(PassingExamplesParsingSuite, Examples) {
   auto path = GetParam().string();
@@ -55,6 +84,35 @@ TEST_P(PassingExamplesParsingSuite, Examples) {
     EXPECT_TRUE(err_log.IsEmpty())
         << "Message schema generation failed:" << std::endl
         << err_log;
+  }
+
+  // Build
+  if (auto query_opt = hyde::Query::Build(*mmod, err_log)) {
+    auto program_build_lambda = [](auto q, auto log) {
+      return hyde::Program::Build(q, log);
+    };
+    std::optional<class hyde::Program> program_opt;
+
+    // Some tests fail this step
+    if (gBuildFailExamples.count(fs::path(path).filename().string())) {
+      ASSERT_DEBUG_DEATH(
+          program_opt = program_build_lambda(*query_opt, err_log), ".*TODO.*");
+    } else {
+      program_opt = program_build_lambda(*query_opt, err_log);
+    }
+
+    if (program_opt) {
+
+      // Save the IR
+      {
+        auto ir_file = std::string(kGeneratedFilesDir) + "/" +
+                       fs::path(path).filename().stem().string() + ".ir";
+        auto ir_out = hyde::FileStream(display_mgr, ir_file);
+        ir_out.os << *program_opt;
+      }
+
+      SUCCEED();
+    }
   }
 }
 
