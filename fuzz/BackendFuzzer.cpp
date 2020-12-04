8fe9e44558cf9499e3d6013c7064c00d9a7428c6
Brad Larsen <brad.larsen@trailofbits.com>
2020-12-03 19:18:18 -0500

    Delete cruft from new fuzzer

--- fuzz/BackendFuzzer.cpp
+++ fuzz/BackendFuzzer.cpp
@@ -69,68 +69,6 @@ static std::optional<hyde::ParsedModule> ParseModule(std::string_view input, con
   return parser.ParseBuffer(input, config);
 }
 
-// static void ParseAndVerify(std::string_view data) {
-//   // First, parse the given data.
-//   hyde::DisplayManager display_manager;
-//   hyde::ErrorLog error_log(display_manager);
-//   hyde::Parser parser(display_manager, error_log);
-//   hyde::DisplayConfiguration config = {
-//       "harness_module",  // `name`.
-//       2,                 // `num_spaces_in_tab`.
-//       true,              // `use_tab_stops`.
-//   };
-//   auto opt_module = parser.ParseBuffer(data, config);
-//   if (!opt_module) {
-//     gStats.num_failed_parses += 1;
-//     // bail out early if no parse
-//     // error_log.Render(std::cerr);
-//     return;
-//   }
-//
-//   gStats.num_successful_parses += 1;
-//   hyde::ParsedModule module = *opt_module;
-//
-//   // Now, pretty-print the parsed module back to a string.
-//   const auto module_string = ParsedModuleToString(module);
-//
-//   // Now, re-parse the pretty-printed string.
-//   hyde::DisplayManager v_display_manager;
-//   hyde::ErrorLog v_error_log(v_display_manager);
-//   hyde::Parser v_parser(v_display_manager, v_error_log);
-//   hyde::DisplayConfiguration v_config = {
-//       "verified_harness_module",  // `name`.
-//       2,                          // `num_spaces_in_tab`.
-//       true,                       // `use_tab_stops`.
-//   };
-//   auto v_opt_module = v_parser.ParseBuffer(data, v_config);
-//
-//   // Finally, make sure the parsed result is equal to what we first parsed (the
-//   // "round-trip" property).
-//   if (!v_opt_module) {
-//     std::cerr << "Failed to re-parse module:\n";
-//     v_error_log.Render(std::cerr);
-//     abort();
-//   }
-//   hyde::ParsedModule v_module = *v_opt_module;
-//   if (!v_error_log.IsEmpty()) {
-//     std::cerr << "Error log is non-empty after reparsing:\n";
-//     v_error_log.Render(std::cerr);
-//     abort();
-//   }
-//   const auto v_module_string = ParsedModuleToString(v_module);
-//   if (module_string != v_module_string) {
-//     std::cerr << "Re-parsed module is not equal to original module:\n"
-//               << "Original module:\n"
-//               << "----------------------\n"
-//               << module_string << "----------------------\n"
-//               << "\n"
-//               << "Re-parsed module:\n"
-//               << "----------------------\n"
-//               << v_module_string << "----------------------\n";
-//     abort();
-//   }
-// }
-
 static hyde::ParsedModule make_ast(unsigned int Seed) {
   // FIXME: actually use the seed!
   // FIXME: do something more interesting here
@@ -141,6 +79,9 @@ static hyde::ParsedModule make_ast(unsigned int Seed) {
 }
 
 }  // namespace
+
+
+
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   std::string_view input(reinterpret_cast<const char *>(Data), Size);
@@ -155,6 +96,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 
   return 0;
 }
+
+
+
+
 
 // Forward-declare the libFuzzer's mutator callback.
 // FIXME: we only need this if we are going to call the default LibFuzzer mutator ourselves.
