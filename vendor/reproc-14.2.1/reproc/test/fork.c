c894a3f7e2a3138364642950e2afd0c53f72bdd1
Brad Larsen <brad.larsen@trailofbits.com>
2020-12-09 16:21:21 -0500

    Add reproc 14.2.1 release sources.
    reproc (https://github.com/DaanDeMeyer/reproc) is a portable library for
    C and C++ for working with subprocesses.  MIT licensed.
    
    These sources came from release 14.2.1 from the releases page on GitHub,
    from commit 8f70f97af263dd3f41bdc07f6f04e92436b1b55a.
    
    The tarball these came from:
    
        $ shasum -a 1 v14.2.1.tar.gz
        9782a7148391e37c04eb844295e9bc66601eae1e  v14.2.1.tar.gz

---
+++ vendor/reproc-14.2.1/reproc/test/fork.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <reproc/run.h>
+
+#include "assert.h"
+
+int main(void)
+{
+  const char *MESSAGE = "reproc stands for REdirected PROCess!";
+  char *output = NULL;
+  reproc_sink sink = reproc_sink_string(&output);
+  int r = -1;
+
+  r = reproc_run_ex(NULL, (reproc_options){ .fork = true }, sink, sink);
+
+  if (r == 0) {
+    printf("%s", MESSAGE);
+    fclose(stdout); // `_exit` doesn't flush stdout.
+    _exit(r < 0 ? EXIT_FAILURE : EXIT_SUCCESS);
+  }
+
+  ASSERT_OK(r);
+  ASSERT(output != NULL);
+  ASSERT_EQ_STR(output, MESSAGE);
+
+  reproc_free(output);
+}
