d568eb87e8b7dad8e184132a73868b9225c2449e
Peter Goodman <peter.goodman@gmail.com>
2021-03-29 15:05:59 -0400

    Compiler error

--- tests/Parsing.cpp
+++ tests/Parsing.cpp
@@ -110,11 +110,14 @@ TEST_P(PassingExamplesParsingSuite, Examples) {
   //
   // Note: Some tests fail to build -- handle those specially
   if (kBuildDebugFailExamples.count(path_filename_str)) {
-    ASSERT_DEBUG_DEATH(hyde::Program::Build(*query_opt, err_log), ".*TODO.*");
+    ASSERT_DEBUG_DEATH(hyde::Program::Build(
+        *query_opt, hyde::IRFormat::kIterative),
+        ".*TODO.*");
     return;
   }
 
-  auto program_opt = hyde::Program::Build(*query_opt, err_log);
+  auto program_opt = hyde::Program::Build(
+      *query_opt, hyde::IRFormat::kIterative);
   ASSERT_TRUE(program_opt.has_value());
 
   auto generated_file_base =
