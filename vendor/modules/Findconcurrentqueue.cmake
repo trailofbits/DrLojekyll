d0ab00e6b438c5225735a14dbe657b42b5c13e76
Alessandro Gario <alessandro.gario@gmail.com>
2021-09-26 22:44:17 +0200

    CMake: Link concurrentqueue against Runtime

---
+++ vendor/modules/Findconcurrentqueue.cmake
@@ -0,0 +1,7 @@
+# Copyright 2020, Trail of Bits, Inc. All rights reserved.
+
+add_subdirectory(
+  "${CMAKE_CURRENT_LIST_DIR}/../concurrentqueue"
+  "${CMAKE_BINARY_DIR}/vendor/concurrentqueue"
+  EXCLUDE_FROM_ALL
+)
