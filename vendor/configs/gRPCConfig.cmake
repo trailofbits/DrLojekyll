d83dc913c3b15f8fd1ace827245d9a23225b8292
Peter Goodman <peter.goodman@gmail.com>
2021-10-05 17:06:44 -0400

    Allow multiple find_packages of gRPC

--- vendor/configs/gRPCConfig.cmake
+++ vendor/configs/gRPCConfig.cmake
@@ -1,7 +1,9 @@
 # Copyright 2020, Trail of Bits, Inc. All rights reserved.
 
-add_subdirectory(
-  "${CMAKE_CURRENT_LIST_DIR}/../grpc"
-  "${CMAKE_BINARY_DIR}/vendor/grpc"
-  EXCLUDE_FROM_ALL
-)
+if(NOT TARGET gRPC::grpc++)
+  add_subdirectory(
+    "${CMAKE_CURRENT_LIST_DIR}/../grpc"
+    "${CMAKE_BINARY_DIR}/vendor/grpc"
+    EXCLUDE_FROM_ALL
+  )
+endif()
